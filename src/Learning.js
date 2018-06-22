c13b0ddc0c4a45cc28263dcfa7ee5886cc612b15
Keaton turner <keatonturner@Keatons-MacBook-Pro.local>
2018-06-22 16:02:49 -0600

    react-changes

---
+++ src/Learning.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Learning extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                 This is my learning compenent.
+                </h1>
+            </div>
+        )
+    }
+}
+export default Learning
---
+++ src/Third.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Third extends Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    I am assuming this is my third component.
+                </h1>
+                </div>
+        )
+    }
+}
+export default Third
